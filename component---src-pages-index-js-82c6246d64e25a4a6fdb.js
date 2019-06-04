b5bccb8689869a696a645007be576fee9e3a339c
Kaisa Huusko <kaisa.huusko@stuntfreaksteam.org>
2019-06-04 09:23:36 +0300

    Updates

---
+++ component---src-pages-index-js-82c6246d64e25a4a6fdb.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{140:function(e,n,t){"use strict";t.r(n);var s=t(0),i=t.n(s);n.default=function(){return i.a.createElement("div",null,"Hello world! This is a test. Test.")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-82c6246d64e25a4a6fdb.js.map
